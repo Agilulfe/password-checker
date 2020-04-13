d70bb205ed676d883e411832ec036bb7ac0d1782
Agilulfe <guillaume.jacobs1995@outlook.com>
2020-04-13 16:02:11 +0200

    type_hints and structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # password-checker
 Use pwnedpasswords.com API to check if your password (encrypted) has ever been leaked
+
+In the shell, run:
+python3 checkpassword.py <password(s)>
--- checkpassword.py
+++ checkpassword.py
@@ -9,31 +9,7 @@ import hashlib
 import sys
 
 
-def request_api_data(query_char):
-    url = 'https://api.pwnedpasswords.com/range/' + query_char
-    res = requests.get(url)
-    if res.status_code != 200:
-        raise RuntimeError(
-            f'Error fetching: {res.status_code}, check the api and try again')
-    return res
-
-
-def get_password_leaks_count(hashes, hash_to_check):
-    hashes = (line.split(':') for line in hashes.text.splitlines())
-    for h, count in hashes:
-        if h == hash_to_check:
-            return count
-    return 0
-
-
-def pwned_api_check(password):
-    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    first5_char, tail = sha1password[:5], sha1password[5:]
-    response = request_api_data(first5_char)
-    return get_password_leaks_count(response, tail)
-
-
-def main(args):
+def main(args: list):
     for password in args:
         count = pwned_api_check(password)
         if count:
@@ -42,6 +18,30 @@ def main(args):
         else:
             print(f'{password} was NOT found. Carry on!')
     return 'done!'
+
+
+def pwned_api_check(password: str):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+
+def request_api_data(first5_char: str):
+    url = 'https://api.pwnedpasswords.com/range/' + first5_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(
+            f'Error fetching: {res.status_code}, check the api and try again')
+    return res
+
+
+def get_password_leaks_count(response: requests.Response, tail: str):
+    response = (line.split(':') for line in response.text.splitlines())
+    for h, count in response:
+        if h == tail:
+            return count
+    return 0
 
 
 if __name__ == '__main__':
